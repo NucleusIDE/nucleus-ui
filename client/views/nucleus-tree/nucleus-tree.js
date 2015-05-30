e25186cd30ac01d9a50314f41e9ad3e3eefacb72
Charanjit Singh <ckhabra007@gmail.com>
2015-05-31 02:10:08 +0530

    Almost done with a basic version of nucleus tree
    Stuck on a stupid problem of hiding all the sub-children on clicking the node, and revealing them in the same state (of their children) on showing again

--- client/views/nucleus-tree/nucleus-tree.js
+++ client/views/nucleus-tree/nucleus-tree.js
@@ -1,44 +1,95 @@
-var treeState = new ReactiveDict();
+var Tree = function(nodes) {
+  this.setNodes(nodes);
+  return this;
+};
+Tree.prototype.setNodes = function(nodes) {
+  var self = this;
+  this.nodes = nodes;
+  this.nodes = nodes.map(function(row) {
+    row.rowClasses = row.rowClasses || '';
+    if (row.hasChildren)  //show icon in front of row with children
+      row.rowClasses += ' nucleus-tree__row--has-children ';
 
-treeState.set("rows", [{
+    //set the left-padding as per row's level
+    var basePadding = 20;
+    var rowLevel = self.getNodeLevel(row);
+    var padding = (rowLevel * 12) + basePadding;
+    row.styles = "padding-left: " + padding + "px;";
+
+    if (row.parentId) {
+      row.rowClasses += ' hidden ';
+    }
+
+    return row;
+  });
+};
+Tree.prototype.findById = function(nodeId) {
+  for(var i = 0; i < this.nodes.length; i++) {
+    if (this.nodes[i].id === nodeId) {
+      return this.nodes[i];
+    }
+  }
+};
+Tree.prototype.getNodeLevel = function(node) {
+  var self = this;
+
+  if (!node.parentId) {
+    return 1;
+  }
+
+  return 1 + self.getNodeLevel(self.findById(node.parentId));
+};
+Tree.prototype.getChildren = function(node) {
+  var children = [];
+
+  for(var i = 0; i < this.nodes.length; i++) {
+    if (this.nodes[i].parentId === node.id) {
+      children.push(this.nodes[i]);
+    }
+  }
+
+  return children;
+};
+
+var tree = new Tree([{
   type: "folder",
+  id: "/.meteor",
   name: ".meteor",
-  children: [1, 2],
-  itemClasses: "",
-  labelClasses: ""
+  hasChildren: true,
+  itemClasses: "explorer-item folder-icon",
+  labelClasses: "explorer-item-label"
+}, {
+  id: '/.meteor/local',
+  type: "folder",
+  name: "local",
+  hasChildren: true,
+  parentId: "/.meteor"
+}, {
+  id: '/.meteor/local/db',
+  name: 'db',
+  type: 'folder',
+  hasChildren: true,
+  parentId: '/.meteor/local'
 }]);
 
-var getRowLevel = function(row) {
-  var level = 1;
-
-  return level;
-};
 
 Template.nucleusTree.helpers({
   treeHeight: function() {
     var baseHeight = 24;
-    return treeState.get('rows').length * baseHeight + 'px';
+    return tree.nodes.length * baseHeight + 'px';
   },
   rows: function() {
-    return treeState.get('rows').map(function(row) {
-
-      if (row.children && row.children.length)  //show icon in front of row with children
-        row.hasChildrenClass = 'nucleus-tree__row--has-children';
-
-      //set the left-padding as per row's level
-      var basePadding = 32;
-      var rowLevel = getRowLevel(row);
-      row.styles = "padding-left: " + rowLevel * basePadding + "px";
-
-      return row;
-    });
+    return tree.nodes;
   },
 });
 
 Template.nucleusTree.events({
   "click .nucleus-tree__row": function(e) {
     var target = $(e.currentTarget);
-    if (target.hasClass('nucleus-tree__row--has-children'))
+    var row = this;
+    if (this.hasChildren)
       target.toggleClass('nucleus-tree__row--expanded');
+
+    $('[data-parent-id="' + this.id + '"]').toggleClass('hidden');
   }
 });
